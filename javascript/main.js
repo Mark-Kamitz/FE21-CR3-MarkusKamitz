e2c782b985ef5e49dccc7c33612c4eec11b98576
markus kamitz <markus_kamitz@yahoo.de>
2021-06-26 08:44:58 +0200

    FE21-CR3-Markus-Kamitz final

---
+++ javascript/main.js
@@ -0,0 +1,81 @@
+// ***** calculateInvoice()  ************************************************************
+
+// function calculateInvoice(starterPrice, maindishPrice, dessertPrice, beveragePrice){
+//     var invoice = starterPrice + maindishPrice + dessertPrice + beveragePrice;
+//     return invoice ;
+// }
+
+// var total = calculateInvoice(30, 30, 20, 20);
+
+// document.write(total);
+// console.log(total);
+
+// ***** 4 parameters for calculateInvoice() *********************************************
+
+// var starters = 9.50;
+// var mainCourse = 11.50;
+// var desserts = 5.50;
+// var beverages = 4.50;
+
+// function calculateInvoice(starterPrice, maindishPrice, dessertPrice, beveragePrice){
+//     var invoice = starterPrice + maindishPrice + dessertPrice + beveragePrice;
+//     return invoice ;
+// }
+
+// var total = calculateInvoice(starters, mainCourse, desserts, beverages);
+// console.log(total);
+
+
+// ***** calculate at least 3 invoices for calculateInvoice()  ******************************
+
+// function calculateInvoice(starterPrice, maindishPrice, dessertPrice, beveragePrice){
+//     var invoice = starterPrice + maindishPrice + dessertPrice + beveragePrice;
+//     return invoice ;
+// }
+
+// var total1 = calculateInvoice(7, 13.50, 7, 22.50);
+// console.log(total1);
+
+// var total2 = calculateInvoice(8.60, 12,5.50, 4.50);
+// console.log(total2);
+
+// var total3 = calculateInvoice(9.20, 14.20, 11.50, 7);
+// console.log(total3);
+
+// ***** studentInvoice() inc. parameters in variables **************************************
+
+// var starters = 8.60;
+// var mainCourse = 13.50;
+// var desserts = 5.50;
+// var beverages = 4.50;
+
+// function studentInvoice(starterPrice, maindishPrice, dessertPrice, beveragePrice){
+//     let percent = 10;
+//     let discountItemsSum = starterPrice + maindishPrice + dessertPrice;
+//     let discountPrice = discountItemsSum - ((10/100) * discountItemsSum);
+//     let invoice = discountPrice + beveragePrice;
+//     return invoice ;
+// }
+
+// var total = studentInvoice(starters, mainCourse, desserts, beverages);
+// console.log(total);
+
+// ***** Calculate at least 3 invoices for studentInvoice()  ***************************************
+
+
+// function studentInvoice(starterPrice, maindishPrice, dessertPrice, beveragePrice){
+//     let percent = 10;
+//     let discountItemsSum = starterPrice + maindishPrice + dessertPrice;
+//     let discountPrice = discountItemsSum - ((10/100) * discountItemsSum);
+//     let invoice = discountPrice + beveragePrice;
+//     return invoice ;
+// }
+
+// var total1 = studentInvoice(20, 30, 10, 5);
+// console.log(total1);
+
+// var total2 = studentInvoice(5, 10, 5, 5);
+// console.log(total2);
+
+// var total3 = studentInvoice(15, 20, 10, 5);
+// console.log(total3);
